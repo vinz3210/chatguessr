50c4dc916bb40caad050d675765598d95205ee1f
Andrei Onea <andreionea@gmail.com>
2024-08-09 23:56:49 +0100

    Dump extenssr postprocessing effects and replace no car effect

--- src/renderer/mods/env.d.ts
+++ src/renderer/mods/env.d.ts
@@ -1,3 +1,5 @@
+import PostProcessingController, { PostProcessingState } from "./extenssr/post_processing_controller"
+
 interface Window {
   toggleNoCarMode: (el: HTMLInputElement) => void
   toggleNoCompassMode: (el: HTMLInputElement) => void
@@ -6,4 +8,6 @@ interface Window {
   changeDelayTime: (el: HTMLInputElement) => void
   toggleSatelliteMode: (el: HTMLInputElement) => void
   changeBoundsLimit: (el: HTMLInputElement) => void
+  pp: PostProcessingState
+  ppController: PostProcessingController | null
 }
